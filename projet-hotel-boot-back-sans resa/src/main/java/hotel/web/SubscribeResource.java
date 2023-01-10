ebce4e442a188c37bded17dfae4ee7c47d7a765a
Sammydoo <huot.clement86@gmail.com>
2023-01-10 21:30:57 +0100

    controleur login et subscribe

---
+++ projet-hotel-boot-back-sans resa/src/main/java/hotel/web/SubscribeResource.java
@@ -0,0 +1,37 @@
+package hotel.web;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import hotel.dao.IDAOCompte;
+import hotel.model.Client;
+
+@RestController
+@RequestMapping("/inscription")
+@CrossOrigin("*")
+public class SubscribeResource {
+	
+	@Autowired
+	private IDAOCompte daoCompte;
+	
+	@PostMapping("")
+	public Client create(@Valid @RequestBody Client client, BindingResult result) {
+		if (result.hasErrors()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Le client n'a pu être créé");
+		}
+		client= daoCompte.save(client);
+		
+		return client;
+		
+	}
+	
+}
