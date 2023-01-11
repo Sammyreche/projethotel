c263136ab0b13da99d3a0f0b592cb4d9a65bf1b8
Sammydoo <huot.clement86@gmail.com>
2023-01-11 09:20:15 +0100

    login repare

--- projet-hotel-boot-back-sans resa/src/main/java/hotel/dao/IDAOCompte.java
+++ projet-hotel-boot-back-sans resa/src/main/java/hotel/dao/IDAOCompte.java
@@ -9,7 +9,7 @@ import hotel.model.Compte;
 public interface IDAOCompte extends JpaRepository<Compte,Integer> {
 	
 	@Query("SELECT c from Compte c where c.login = :login and c.password= :pasword")
-	public List<Compte> findByLoginAndPassword(String login, String password);
+	public Compte findByLoginAndPassword(String login, String password);
 
 }
 
