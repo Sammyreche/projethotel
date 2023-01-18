68ff9f873de7e69a10e34baefbce7b08178a5fae
Joanna <joanna.njeim@hotmail.com>
2023-01-18 10:37:03 +0100

    rajout listeresa nav

--- Projer-hotel-front-angular/HotelFrontAngular/src/app/inscription/inscription.service.ts
+++ Projer-hotel-front-angular/HotelFrontAngular/src/app/inscription/inscription.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppConfigService } from '../app-config.service';
+import { ConnexionService } from '../connexion/connexion.service';
 import { Client } from '../models/compte.model';
 
 
@@ -13,7 +14,7 @@ export class InscriptionService {
   serviceUrl! : string;
   
 
-  constructor(private http: HttpClient, private appConfig: AppConfigService, private router:Router) {
+  constructor(private http: HttpClient, private appConfig: AppConfigService, private router:Router, private connexionService: ConnexionService) {
 
     this.serviceUrl = appConfig.backEndUrl + "inscription";
     //this.load();
@@ -24,6 +25,10 @@ export class InscriptionService {
       console.log(inscription)
       this.http.put<Client>(this.serviceUrl, inscription).subscribe(resp => {
         console.log(inscription);
+      if (this.connexionService.compteConnecte) {
+        this.router.navigate(['listeResa']);
+      }
+      else
         this.router.navigate(['login'], { queryParams :{'username': inscription.login}});
        });
        
